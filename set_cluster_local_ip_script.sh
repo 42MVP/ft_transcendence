81504b97d5e77c9c0b27788714271779517596cd
Chaejin Kim <73324850+chaejin-jen@users.noreply.github.com>
2023-09-08 18:50:48 +0900

    ✨ FEAT: env sever url (#7)
    * 🔧CONF: Fix postgreSQL docker image as latest
    
    * ✨FEAT: Add script for cluster local ip

---
+++ set_cluster_local_ip_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IP_ADDRESS=$(ipconfig getifaddr en0)
+
+sed -i '' "s|VITE_APP_API_URL=.*|VITE_APP_API_URL=http://$IP_ADDRESS:3000|" frontend/.env
+sed -i '' "s|42API_REDIRECT_URI = .*|42API_REDIRECT_URI = http://$IP_ADDRESS:3000/login/redirect|" backend/.env
+
+echo "VITE_APP_API_URL updated to http://$IP_ADDRESS:3000"
